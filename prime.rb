07764d0c1ce16789736e844c197f9474da5f6d67
Anjana Nair <anju740@gmail.com>
2019-06-19 01:48:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,16 +6,18 @@ def prime?(num)
   numset.none? { |number| num % number == 0}
 end
 
-#def prime?(num)
-#  prime = true
-#  if num < 2
-#    return false
-#  end
-#  numset = Array(2..num-1)
-#  numset.each do |number|
-#    if num % number == 0
-#      prime = false
-#    end
-#  end
-#  prime
-#end
+=begin
+def prime?(num)
+  prime = true
+  if num < 2
+    return false
+  end
+  numset = Array(2..num-1)
+  numset.each do |number|
+    if num % number == 0
+      prime = false
+    end
+  end
+  prime
+end
+=end
