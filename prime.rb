6ad8dd4e50abb55773cf5236546b4ed09755eb66
Anjana Nair <anju740@gmail.com>
2019-06-19 01:44:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -11,3 +11,17 @@ def prime?(num)
   end
   prime
 end
+
+#def prime?(num)
+#  prime = true
+#  if num < 2
+#    return false
+#  end
+#  numset = Array(2..num-1)
+#  numset.each do |number|
+#    if num % number == 0
+#      prime = false
+#    end
+#  end
+#  prime
+#end
